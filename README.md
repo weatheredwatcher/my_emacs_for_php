46081041b543cf653556fe286e075bb04c3eb11f
David Duggins <weatheredwatcher@gmail.com>
2012-11-15 12:32:59 -0500

    full commit of all the base files

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 my_emacs_for_php
 ================
 
-This is my personal emacs configs, tuned for php development
+This is my personal emacs configs, tuned for php development.
+
+I got the idea from a Rails developer, Spastino.  He stopped updating his files three years ago.  Realistically, a lot of the stuff that I use is installed via defaults in the package manager...so this is just a few extra things that I use.  It also includes the theme that I use, Solarized. (both light and dark versions)  I also keep some little tweaks in here that I prefer in my emacs.
---
+++ init.el
@@ -0,0 +1,18 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized-master")
+(load-theme 'solarized-dark' t)
+(setq stack-trace-on-error t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40")
+ '(spice-output-local "Gnucap")
+ '(spice-simulator "Gnucap")
+ '(spice-waveform-viewer "Gwave"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
